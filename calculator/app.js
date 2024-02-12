f1a4b7e948a81f0630dbb7d529a0920eae635585
SAAD HUSSAIN <143199003+SAADHUSSAIN31821@users.noreply.github.com>
2024-02-13 03:19:27 +0500

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,23 @@
+function appendToDisplay(value) {
+    var display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    var display = document.getElementById('display');
+    display.value = '';
+}
+
+function deleteLast() {
+    var display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    var display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
